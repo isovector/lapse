ec3aed4d1a5eb8c3388f4f05611a5a816d8f53df
Sandy Maguire <sandy@sandymaguire.me>
2022-08-05 01:18:16 -0700

    feat: options, run in a loop

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,34 +1,67 @@
+{-# OPTIONS_GHC -Wall #-}
+
 module Lib where
 
 import ClockPanel
 import Control.Concurrent.Async
 import Data
-import Data.Time (fromGregorian)
-import Data.Time.LocalTime
 import Diagrams.Backend.Rasterific (renderRasterific)
-import Diagrams.Prelude
+import Diagrams.Prelude hiding (value, Options)
 import GitPanel
 import SpyPanels
 import System.IO.Temp (withSystemTempDirectory)
 import Utils
+import Options.Applicative
+import System.Process (readProcess)
+import System.Directory (getDirectoryContents)
+import Control.Monad (forever)
+
+
+data Options = Options
+  { o_sha :: Maybe String
+  , o_dir :: FilePath
+  }
+
+
+optsParser :: ParserInfo Options
+optsParser =
+  info (helper <*> versionOption <*> programOptions) $ mconcat
+    [ fullDesc
+    , header "lapse - make programming timelapses"
+    ]
+
+
+versionOption :: Parser (a -> a)
+versionOption = infoOption "1.0" $ long "version" <> help "Show version"
+
+
+programOptions :: Parser Options
+programOptions =
+  Options
+    <$> optional (strOption (long "sha" <> metavar "SHA" <> help "SHA to base git stats relative to"))
+    <*> argument str (metavar "OUTPUT-DIR" <> help "Output directory")
 
 
 main :: IO ()
 main = do
-    let start = LocalTime (fromGregorian 2022 8 4) $ TimeOfDay 21 0 0
+  opts <- execParser optsParser
+  sha <- maybe getSha pure $ o_sha opts
+
+  forever $ do
     withSystemTempDirectory "lapse" $ \dir -> do
-      (screen, webcam, now, gs, ss) <- runConcurrently $
-        (,,,,)
+      (screen, webcam, now, gs, ss, fp) <- runConcurrently $
+        (,,,,,)
           <$> Concurrently (getScreenshot dir)
           <*> Concurrently (getWebcam dir)
           <*> Concurrently getNow
-          <*> Concurrently (getGitStats "a1d41c8b00057d415ac96369f557f67fa4134846")
+          <*> Concurrently (getGitStats sha)
           <*> Concurrently getSelfStats
+          <*> Concurrently (getNextFileName $ o_dir opts)
       let panel_sep = rect 2 240 # fc grey # lw 0
-          bg = rect 1920 240 # fc (darken 0.05 darkviolet) # lw 0
-      renderRasterific "/tmp/out.png" (dims $ V2 1920 (1080 + 240)) $  vcat
+          background = rect 1920 240 # fc (darken 0.05 darkviolet) # lw 0
+      renderRasterific fp(dims $ V2 1920 (1080 + 240)) $  vcat
         [ screen
-        , flip mappend bg $ centerX $ hcat' (def & sep .~ 40) $ fmap centerY
+        , flip mappend background $ centerX $ hcat' (def & sep .~ 40) $ fmap centerY
             [ spyPanel ss
             , panel_sep
             , gitPanel gs
@@ -38,4 +71,20 @@ main = do
             , workPanel now ss
             ]
         ]
+
+
+getNextFileName :: FilePath -> IO FilePath
+getNextFileName dir = do
+  sz <- fmap length $ getDirectoryContents dir
+  pure $ dir <> "/" <> lpad 10 '0' (show sz) <> ".png"
+
+
+lpad :: Int -> Char -> String -> String
+lpad sz c s = replicate (sz - length s) c <> s
+
+
+getSha :: IO String
+getSha
+  = fmap (head . filter (not . null) . lines)
+  $ readProcess "git" ["rev-parse", "HEAD"] ""
 
